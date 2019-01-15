d94ed0d38ad98db04ed31ea6424add1dd43e4418
Arthur Christian <32134423+achristian91@users.noreply.github.com>
2019-01-15 01:43:28 -0500

    Updates

---
+++ precache-manifest.5a4898ddd505a116eb6d27299f9c913f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f0724a0a392b4ba4d33",
+    "url": "/memorygame/static/js/runtime~main.1f0724a0.js"
+  },
+  {
+    "revision": "6df7618755f332b82985",
+    "url": "/memorygame/static/js/main.6df76187.chunk.js"
+  },
+  {
+    "revision": "abebb7a896f3470ffe4a",
+    "url": "/memorygame/static/js/1.abebb7a8.chunk.js"
+  },
+  {
+    "revision": "6df7618755f332b82985",
+    "url": "/memorygame/static/css/main.372f83c4.chunk.css"
+  },
+  {
+    "revision": "ebaf81d9c47df190585f6218bbbaafe5",
+    "url": "/memorygame/index.html"
+  }
+];
